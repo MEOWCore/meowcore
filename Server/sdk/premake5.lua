838381b10f9da7147814e760d113c0c85f2aeda3
Adam <vobla@unix-net.ru>
2016-12-25 14:59:27 +0800

    Another changes to premake scripts.

---
+++ Server/sdk/premake5.lua
@@ -0,0 +1,26 @@
+project "Server - SDK"
+	language "C++"
+	kind "StaticLib"
+	targetname "sdk"
+	
+	pchheader "StdInc.h"
+	pchsource "StdInc.cpp"
+	
+	flags { "ExcludeFromBuild" }
+
+	vpaths { 
+		["Headers/*"] = { "**.h", "**.hpp" },
+		["Sources/*"] = "**.cpp",
+		["*"] = "premake5.lua"
+	}
+	
+	includedirs {
+		"../../vendor/sparsehash/current/src/"
+	}
+	
+	files {
+		"premake5.lua",
+		"**.h",
+		"**.hpp",
+		"**.cpp"
+	}
